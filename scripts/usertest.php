9976aad40dc9d308e804dd689feaaac3bc49e1d5
JustBrenkman <JustBrenkman@gmail.com>
2018-03-03 14:42:50 -0700

    Added some php code to send a test email and also added a example feedback form at the bottom of the home page.

---
+++ scripts/usertest.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ben Brenkman
+ * Date: 2/28/2018
+ * Time: 9:04 PM
+ */
+
+
+
+?>
---
+++ sendFeedback.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ben Brenkman
+ * Date: 3/3/2018
+ * Time: 10:16 AM
+ */
+
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['feedback'])) {
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $feedback = $_POST['feedback'];
+
+    //$to = "feedback@timpanogos-tech.com";
+    $to = "register_request@timpanogos-tech.com";
+    $from = $email;
+    $message = '<b>Name: </b>'.$name.'<br><b>Email: </b>'.$email.'<br><p>'.$feedback.'</p>';
+    $headers = "From: $from\n";
+    $headers .= "MIME-Version: 1.0\n";
+    $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+
+    if (mail($to, $from, $message, $headers)) {
+        echo "success";
+    } else {
+        echo "failed :(";
+    }
+} else {
+    echo "Failed";
+}
+
+?>
