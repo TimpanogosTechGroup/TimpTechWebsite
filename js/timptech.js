f3a32f22bc9b9f8b041d99b36441211934d0b530
JustBrenkman <JustBrenkman@gmail.com>
2018-03-04 21:35:41 -0700

    Modified the footer on all the pages, added the website feedback modal

---
+++ js/timptech.js
@@ -0,0 +1,23 @@
+function _(id) { return document.getElementById(id);}
+
+function submitFeedback() {
+    _("submitFeedbackbtn").disabled = true;
+    _("submitFeedbackbtn").innerHTML = "Submitting, please wait...";
+    var formData = new FormData();
+    formData.append("name", _("name").value);
+    formData.append("email", _("email").value);
+    formData.append("feedback", _("textFeedback").value);
+    var ajax = new XMLHttpRequest();
+    ajax.open("POST", "sendFeedback.php");
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState === 4 && ajax.status === 200) {
+            if (ajax.responseText === "success") {
+                _("feedbackForm").innerHTML = "<h2> Successfully sent :)</h2>";
+                _("submitFeedbackbtn").innerHTML = ajax.responseText;
+            } else {
+                _("submitFeedbackbtn").disabled = false;
+            }
+        }
+    };
+    ajax.send(formData);
+}
