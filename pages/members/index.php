b4686c10deb220554b0c8e049d0ec70a389f71cf
JustBrenkman <JustBrenkman@gmail.com>
2018-03-20 15:34:36 -0600

    Made some updates to the Join, Team and Member pages.

--- pages/members/index.php
+++ pages/members/index.php
@@ -56,6 +56,12 @@ session_start();
                            href="https://www.facebook.com/Timpanogos-Tech-560674584311834/" target="_blank"
                            data-original-title="Like us on Facebook"><i class="fa fa-facebook-square"></i></a>
                     </li>
+                    <li class="nav-item">
+                        <button class="btn btn-round" data-toggle="modal" data-target="#loginModal">
+                            Login<i class="material-icons">assignment</i>
+
+                        </button>
+                    </li>
                 </ul>
             </div>
         </div>
@@ -185,6 +191,58 @@ Website code goes above this section
                 <button type="button" onclick="submitFeedback(); return false;" id="submitFeedbackbtn"
                         class="btn btn-primary">SEND
                 </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="loginModal" tabindex="-1" role="">
+    <div class="modal-dialog modal-login" role="document">
+        <div class="modal-content">
+            <div class="card card-signup card-plain">
+                <div class="modal-header">
+                    <div class="card-header card-header-primary text-center">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="material-icons">clear</i></button>
+                        <h4 class="card-title">Log in</h4>
+                        <div class="social-line">
+                            <a href="#pablo" class="btn btn-just-icon btn-link">
+                                <i class="fa fa-facebook-square"></i>
+                            </a>
+                            <a href="#pablo" class="btn btn-just-icon btn-link">
+                                <i class="fa fa-twitter"></i>
+                            </a>
+                            <a href="#pablo" class="btn btn-just-icon btn-link">
+                                <i class="fa fa-google-plus"></i>
+                                <div class="ripple-container"></div></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-body">
+                    <form class="form" method="post">
+                        <div class="card-body align-content-center">
+                            <div class="form-group">
+                                <div class="input-group">
+<!--                                    <span class="input-group-addon">-->
+<!--                                        <i class="material-icons">email</i>-->
+<!--                                    </span>-->
+                                    <input type="text" name="username" class="form-control" placeholder="User Name...">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="input-group">
+<!--                                    <span class="input-group-addon">-->
+<!--                                        <i class="material-icons">lock_outline</i>-->
+<!--                                    </span>-->
+                                    <input type="password" placeholder="Password..." class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer justify-content-center">
+                    <a href="#pablo" class="btn btn-primary btn-link btn-wd btn-lg">Get Started</a>
+                </div>
             </div>
         </div>
     </div>
